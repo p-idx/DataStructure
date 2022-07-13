81756f8bdcadd36adb7b8c5ff930999f77881587
cubeindex <cubeindex@DESKTOP-I2A60GM>
2022-07-14 02:57:27 +0900

    Implement Insert func.

--- Tree/BST/main.cpp
+++ Tree/BST/main.cpp
@@ -21,8 +21,8 @@ class TreeNode
 	friend class BST;
 protected:
 	T data;
-	TreeNode<T>* left;
-	TreeNode<T>* right;
+	TreeNode<T>* left = nullptr;
+	TreeNode<T>* right = nullptr;
 };
 
 template <typename K, typename E>
@@ -42,28 +42,116 @@ public:
 
 	std::pair<K, E>* Get(TreeNode <std::pair<K, E>>* p, const K& k)
 	{
+		if (!p) return nullptr;
+		else if (k < p->data.first) return Get(p->left, k);
+		else if (k > p->data.first) return Get(p->right, k);
 		return &(p->data);
 	}
 
 	virtual std::pair<K, E>* Get(const K& k)
 	{
-		return this->Get(this->root, k);
+		return Get(root, k);
 	}
 
-	virtual void Insert(const std::pair<K, E>&)
+	virtual void Insert(const std::pair<K, E>& thePair)
 	{
+		TreeNode<std::pair<K, E>>* p = root, * pp = 0;
+		while (p)
+		{
+			pp = p;
+			if (thePair.first < p->data.first) p = p->left;
+			else if (thePair.first > p->data.first) p = p->right;
+			else // 이미 키가 있으면 그 값을 변경
+			{
+				p->data.second = thePair.second;
+			}
+		}
 
+		p = new TreeNode<std::pair<K, E>>;
+		p->data.first = thePair.first;
+		p->data.second = thePair.second;
+		if (root) // 트리가 공백이 아닐 때
+		{
+			if (thePair.first < pp->data.first) pp->left = p;
+			else
+				pp->right = p;
+		}
+		else
+			root = p;
 	}
+
 	virtual void Delete(const K&)
 	{
 
 	}
+
+	void Inorder()
+	{
+		std::cout << "Inorder Print" << std::endl;
+		Inorder(root);
+	}
+
+	void Preorder()
+	{
+		std::cout << "Preorder Print" << std::endl;
+		Preorder(root);
+	}
+
+	void Inorder(TreeNode<std::pair<K, E>>* node)
+	{
+		if (node)
+		{
+			Inorder(node->left);
+			std::cout << "Key: " << node->data.first << "  Val: " << node->data.second << std::endl;
+			Inorder(node->right);
+		}
+	}
+
+	void Preorder(TreeNode<std::pair<K, E>>* node)
+	{
+		if (node)
+		{
+			std::cout << "Key: " << node->data.first << "  Val: " << node->data.second << std::endl;
+			Preorder(node->left);
+			Preorder(node->right);
+		}
+	}
+
 };
+
 int main()
 {
 	using namespace std;
 	BST<int, int> bst;
-	cout << "Test" << endl;
+
+	pair<int, int> p;
+	p.first = 30;
+	p.second = 1;
+	bst.Insert(p);
+	
+	p.first = 5;
+	p.second = 1;
+	bst.Insert(p);
+
+	p.first = 2;
+	p.second = 1;
+	bst.Insert(p);
+
+	p.first = 40;
+	p.second = 1;
+	bst.Insert(p);
+
+	p.first = 80;
+	p.second = 1;
+	bst.Insert(p);
+
+	p.first = 35;
+	p.second = 1;
+	bst.Insert(p);
+
+
+	bst.Inorder();
+	bst.Preorder();
 
 	return 0;
 }
